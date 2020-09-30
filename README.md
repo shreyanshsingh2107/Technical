3f0cd9ecbc1b090f5b7c98602692cfa0f5059a70
Shreyansh Singh <shreyanshsingh2107@gmail.com>
2020-09-30 19:48:59 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Technical
+
+##  To develop a menu driven project for all types of administrative control for training/placement department of Galgotias University such as
+
+a. Students
+
+b. Trainer
+
+c. Trainer Lead
+
+d. Project Manager
+
+e. Project Head
+
+f. Director
+
+g. ProVC
+
+ 
---
+++ project4.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int  Students(){
+
+}
+
+int  Trainer(){
+
+}
+
+int  TrainerLead(){
+
+}
+
+int  ProjectManager(){
+
+}
+
+int  ProjectHead(){
+
+}
+
+int  Director(){
+
+}
+
+int  ProVC(){
+
+}
+
+int main(){
+    int choice, num; 
+    printf("Press 1 to Check Students\n"); 
+    printf("Press 2 to Check Trainer\n");
+    printf("Press 3 to Check TrainerLead\n");
+    printf("Press 4 to Check ProjectManager\n");
+    printf("Press 5 to Check ProjectHead\n");
+    printf("Press 6 to Check Director\n");
+    printf("Press 7 to Check ProVc\n");
+
+    printf("Enter your choice: "); 
+    scanf("%d",&choice);
+
+    switch (choice)
+    {
+    case 1:
+        
+        break;
+    case 2:
+        
+        break;
+    case 3:
+        
+        break;
+    case 4:
+        
+        break;
+    case 5:
+        
+        break; 
+    case 6:
+        
+        break;
+    case 7:
+        
+        break;
+    
+    default:
+        printf("INVALID CHOICE");
+        break;
+    }
+
+    return 0;
+}
